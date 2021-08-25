f604a9d93a499c0a503b7f3504f5827b16795ea3
AntGovea <teckblendxd@gmail.com>
2021-08-24 19:59:33 -0500

    journlaApp con sass

---
+++ journal-app/journal-app/src/components/RegisterScreen.js
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export const RegisterScreen = () => {
+    return (
+        <div>
+             <div>
+            <h1>Register Screen</h1>
+        </div>
+        </div>
+    )
+}
